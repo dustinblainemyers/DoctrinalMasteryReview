bfc0a0c09ce3e5469b1dc4796deb42a51086d164
Dustin Myers <dustinblaine.myers@gmail.com>
2020-08-27 19:48:19 -0400

    select category works

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
-import React, { useState, useEffect, useRef } from 'react'
+import React, { useState } from 'react'
 import FlashcardList from './FlashcardList'
 import './App.css'
-import axios from 'axios'
+
+const questions = require('./jsontest.json')
 
 function App () {
   const [flashcards, setFlashcards] = useState([])
-  const [categories, setCategories] = useState([])
+  const [value, setValue] = useState([])
+  const categoryKey = []
+  questions.forEach(questions => {
+    const category = questions.category
+    if (categoryKey.indexOf(category) === -1) {
+      categoryKey.push(category)
+    }
+  })
 
-  const categoryEl = useRef()
-  const amountEl = useRef()
-
-  useEffect(() => {
-    axios.get('https://opentdb.com/api_category.php').then(res => {
-      setCategories(res.data.trivia_categories)
-    })
-  }, [])
-
-  useEffect(() => {}, [])
+  console.log(categoryKey)
 
   function decodeString (str) {
     const textArea = document.createElement('textarea')
@@ -24,32 +23,28 @@ function App () {
     return textArea.value
   }
 
+  function handleCategoryChange (e) {
+    e.preventDefault()
+    setValue(e.target.value)
+  }
+
   function handleSubmit (e) {
     e.preventDefault()
-    axios
-      .get('https://opentdb.com/api.php', {
-        params: {
-          amount: amountEl.current.value,
-          category: categoryEl.current.value
-        }
-      })
-      .then(res => {
-        setFlashcards(
-          res.data.results.map((questionItem, index) => {
-            const answer = decodeString(questionItem.correct_answer)
-            const options = [
-              ...questionItem.incorrect_answers.map(a => decodeString(a)),
-              answer
-            ]
-            return {
-              id: `${index}-${Date.now()}`,
-              question: decodeString(questionItem.question),
-              answer: answer,
-              options: options.sort(() => Math.random() - 0.5)
-            }
-          })
-        )
-      })
+
+    questions.forEach((questions, index) => {
+      if (questions.category === value) {
+        const answer = decodeString(questions.answer)
+        const question = decodeString(questions.question)
+        setFlashcards([
+          ...flashcards,
+          {
+            id: `${index}-${Date.now()}`,
+            answer: answer,
+            question: question
+          }
+        ])
+      }
+    })
   }
 
   return (
@@ -57,29 +52,16 @@ function App () {
       <form className='header' onSubmit={handleSubmit}>
         <div className='form-group'>
           <label htmlFor='category'>Category</label>
-          <select id='category' ref={categoryEl}>
-            {categories.map(category => {
-              return (
-                <option value={category.id} key={category.id}>
-                  {category.name}
-                </option>
-              )
+          <select id='category' value={value} onChange={handleCategoryChange}>
+            <option value='Select Category'>Select Category</option>
+            {categoryKey.map(category => {
+              return <option value={category}>{category}</option>
             })}
           </select>
         </div>
+
         <div className='form-group'>
-          <label htmlFor='amount'>Number of Questions</label>
-          <input
-            type='number'
-            id='amount'
-            min='1'
-            step='1'
-            defaultValue={10}
-            ref={amountEl}
-          />
-        </div>
-        <div className='form-group'>
-          <button className='btn'>Generate</button>
+          <button className='btn'>Show Cards</button>
         </div>
       </form>
       <div className='container'>
